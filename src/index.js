304ba52072b6499f8a17bb273281abe7c972475e
Arthur Grenier <arthur.grenier@webdrone.fr>
2022-10-17 11:56:42 +0200

    let instead var ; create div result

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 async function getWeather() {
+    // delete old res
+    if (document.getElementById("res")) {
+        document.removeChild(document.getElementById("res"));
+    }
+
+    // var
     const token = "7ff3737ebec73b955383801130a48541b060f0ab5c630b3209bdb1fe67bd00d7";
     const weatherApi = "api.meteo-concept.com";
-    var city = document.getElementById("cityName").value;
-    var zipCode = document.getElementById("zipCode").value;
-    var element = document.getElementById("setWeather");
+    let city = document.getElementById("cityName").value;
+    let zipCode = document.getElementById("zipCode").value;
+    const uiDiv = document.getElementById("ui");
 
+    // request
     let url = "https://"+weatherApi+"/api/forecast/nextHours?token="+token+"&latlng=47.3229,5.0379&insee=21231&hourly=true";
     const response = await fetch(url);
     const weatherJson = await response.json();
 
+    // parse
     let date = new Date(weatherJson.forecast[0].datetime);
     console.log(date.getHours());
-    element.innerHTML = "Température dans une heure => " + weatherJson.forecast[1].temp2m + "°C"
+
+    // insert res
+    let divRes = document.createElement('div');
+    divRes.setAttribute("id", "res");
+    document.body.appendChild(divRes);
 }
