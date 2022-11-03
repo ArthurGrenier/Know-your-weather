5b8f36070fe6cb8004014d595d2e68e2603a25f7
ArthrGrenier <argnr@proton.me>
2022-11-03 14:29:13 +0100

    #19 Added scroll up button

--- src/index.js
+++ src/index.js
@@ -270,3 +270,18 @@ async function getWeather() {
         divRes.appendChild(titleCity);
     }
 }
+
+window.onscroll = function() {
+    const mybutton = document.getElementById("topButton");
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }    
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+  } 
